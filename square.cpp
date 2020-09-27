87b49eeac8f87a89e3e3369e7ac4172410677553
Antonio Papa <antoniogpapa@gmail.com>
2020-09-27 15:21:37 -0400

    Allows Square to work with strings

--- square.cpp
+++ square.cpp
@@ -1,16 +1,21 @@
- #include <cstdint>
+#include <cstdint>
+#include <string>
 
 class Square {
 
     public:
         Square() {}
 
-        Square(int row, int col) {
+        Square(const int row, const int col) {
             square = row * 8 + col;
         };
 
         Square(std::uint8_t s) {
             square = s;
+        };
+
+        Square(std::string s) {
+            square = (s[1] - '1') * 8 + (s[0] - 'a');
         };
 
         int get_row() const {
@@ -23,6 +28,13 @@ class Square {
 
         int get_int_value() const {
             return square;
+        }
+
+        std::string to_string() const {
+            std::string result;
+            result += 'a' + get_col();
+            result += '1' + get_row();
+            return result;
         }
 
     private:
--- square_test.cpp
+++ square_test.cpp
@@ -2,12 +2,38 @@
 
 #include <gtest/gtest.h>
 
-TEST(SquareTest, row) {
+TEST(SquareTest, get_row) {
     Square s = Square(4, 0);
     ASSERT_EQ(4, s.get_row());
 
     s = Square(2, 0);
     ASSERT_EQ(2, s.get_row());
+}
+
+TEST(SquareTest, get_col) {
+    Square s = Square(0, 4);
+    ASSERT_EQ(4, s.get_col());
+
+    s = Square(0, 2);
+    ASSERT_EQ(2, s.get_col());
+}
+
+TEST(SquareTest, constructor_string) {
+    Square s = Square("d7");
+    ASSERT_EQ(6, s.get_row());
+    ASSERT_EQ(3, s.get_col());
+
+    s = Square("g1");
+    ASSERT_EQ(0, s.get_row());
+    ASSERT_EQ(6, s.get_col());
+}
+
+TEST(SquareTest, to_string) {
+    Square s = Square(0, 0);
+    ASSERT_EQ("a1", s.to_string());
+
+    s = Square(2, 4);
+    ASSERT_EQ("e3", s.to_string());
 }
 
 
