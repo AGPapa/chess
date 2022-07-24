57cba14a8f71bc972dd13b5c8a8d2500b9aa11a4
Antonio Papa <antoniogpapa@gmail.com>
2022-07-24 08:42:00 -0400

    Updates square to follow variable conventions

--- src/chess/square.cpp
+++ src/chess/square.cpp
@@ -8,36 +8,36 @@ class Square {
     public:
         constexpr Square() {}
 
-        constexpr Square(std::uint8_t s) : square(s) {};
+        constexpr Square(std::uint8_t s) : _square(s) {};
 
-        constexpr Square(const int row, const int col) : square(row * 8 + col) {};
+        constexpr Square(const int row, const int col) : _square(row * 8 + col) {};
 
         Square(std::string s) {
-            square = (s[1] - '1') * 8 + (s[0] - 'a');
+            _square = (s[1] - '1') * 8 + (s[0] - 'a');
         };
 
         constexpr int get_row() const {
-            return square / 8;
+            return _square / 8;
         }
  
         constexpr int get_col() const {
-            return square % 8;
+            return _square % 8;
         }
 
         constexpr int get_int_value() const {
-            return square;
+            return _square;
         }
 
         constexpr Square mirror() const {
-            return Square(((7 - (square / 8)) * 8) + (square % 8));
+            return Square(((7 - (_square / 8)) * 8) + (_square % 8));
         }
 
         constexpr bool operator==(const Square& other) const {
-            return square == other.square;
+            return _square == other._square;
         }
 
         constexpr bool operator!=(const Square& other) const {
-            return square != other.square;
+            return _square != other._square;
         }
 
         std::string to_string() const {
@@ -48,5 +48,5 @@ class Square {
         }
 
     private:
-        std::uint8_t square = 0;
+        std::uint8_t _square = 0;
 };
